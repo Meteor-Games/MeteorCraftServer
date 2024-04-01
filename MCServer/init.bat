d1fcbf003ffe3c90ecf814082f253f0c2ed3a7ad
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2024-03-31 22:19:46 -0300

    numa removed

--- MCServer/init.bat
+++ MCServer/init.bat
@@ -1,3 +1,2 @@
-cd C:\Users\Marcos\Documents\GitHub\MeteorCraftServer
-java -Xmx8G -Xms8G -XX:+UseG1GC -XX:ParallelGCThreads=8 -XX:+UseCompressedOops -XX:+UseStringDeduplication -XX:CICompilerCount=4 -XX:+UseLargePages -XX:+UseNUMA -XX:+UseNUMAInterleaving -XX:+UseBiasedLocking -XX:+AggressiveOpts -jar kcauldron.jar nogui
+java -Xmx8G -Xms8G -XX:+UseG1GC -XX:ParallelGCThreads=8 -XX:+UseCompressedOops -XX:+UseStringDeduplication -XX:CICompilerCount=4 -XX:+UseBiasedLocking -XX:+AggressiveOpts -jar kcauldron.jar nogui
 pause
